968fae4cb4e1cf0c93b3379804b444b8ae55b3bc
haestflod <haestflod@gmail.com>
2018-03-24 00:23:46 +0200

    Added Unit Tests for localizedrouteconvention
    For #11

---
+++ localization.tests/UnitTests/Localization/LocalizedRouteConventionTest.cs
@@ -0,0 +1,28 @@
+﻿using localization.Localization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalizationTests.UnitTests.Localization
+{
+    [TestClass]
+    public class LocalizedRouteConventionTest
+    {
+        [TestMethod]
+        public void GetParameterNameTest()
+        {
+            LocalizedRouteConvention routeConvention = new LocalizedRouteConvention();
+
+            List<string> input = new List<string>() { "{action}", "{ CoNtRoLLeR }", "{ moo:id = 1337}", "{howl=1337}" };
+            List<string> expectedOutput = new List<string>() { "action", "controller", "moo", "howl" };
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string name = routeConvention.GetParameterName(input[i]);
+
+                Assert.AreEqual(expectedOutput[i], name);
+            }
+        }
+    }
+}
